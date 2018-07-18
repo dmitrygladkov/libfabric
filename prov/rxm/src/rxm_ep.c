d6ecee988cf750322a0ac4c0bb8e0f3aac589e6f
Dmitry Gladkov <dmitry.gladkov@intel.com>
2018-07-18 16:48:29 +0300

    prov/rxm: Use SAR instead of Rendezvous if MR cache isn't enabled
    Signed-off-by: Dmitry Gladkov <dmitry.gladkov@intel.com>

--- prov/rxm/src/rxm_ep.c
+++ prov/rxm/src/rxm_ep.c
@@ -408,6 +408,9 @@ static int rxm_ep_txrx_res_open(struct rxm_ep *rxm_ep,
 			rxm_ep->sar_limit = param;
 	} else {
 		rxm_ep->sar_limit = RXM_SAR_LIMIT;
+		if (!getenv("FI_VERBS_MR_CACHE_ENABLE")) {
+			rxm_ep->sar_limit = (size_t)-1;
+		}
 	}
 
 	return FI_SUCCESS;
