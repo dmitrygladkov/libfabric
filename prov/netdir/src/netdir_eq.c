798fccec51c12b828facb177dc41dc5ccd3f1756
Sergey E Losev <sergey.e.losev@intel.com>
2018-08-27 14:44:02 +0300

    prov/netdir: implemented completion queue helpers

--- prov/netdir/src/netdir_eq.c
+++ prov/netdir/src/netdir_eq.c
@@ -86,11 +86,24 @@ int ofi_nd_eq_open(struct fid_fabric *fabric, struct fi_eq_attr *attr,
 	if (!nd_eq_ptr)
 		return -FI_ENOMEM;
 
+	/*
 	nd_eq_ptr->fid.fid.fclass = FI_CLASS_EQ;
 	nd_eq_ptr->fid.fid.context = context;
 	nd_eq_ptr->fid.fid.ops = &ofi_nd_fi_ops;
 	nd_eq_ptr->fid.ops = &ofi_nd_eq_ops;
-	nd_eq_ptr->count = 0;
+	*/
+	nd_eq_t def = {
+		.fid = {
+			.fid = {
+				.fclass = FI_CLASS_EQ,
+				.context = context,
+				.ops = &ofi_nd_fi_ops
+			},
+			.ops = &ofi_nd_eq_ops
+		}
+	};
+
+	*nd_eq_ptr = def;
 
 	InitializeCriticalSection(&nd_eq_ptr->lock);
 
@@ -126,18 +139,6 @@ static int ofi_nd_eq_close(struct fid *fid)
 
 	free(eq_ptr);
 	return FI_SUCCESS;
-}
-
-static ssize_t ofi_nd_eq_read(struct fid_eq *peq, uint32_t *pev,
-			      void *buf, size_t len, uint64_t flags)
-{
-	return -FI_ENOSYS;
-}
-
-static ssize_t ofi_nd_eq_readerr(struct fid_eq *peq,
-				 struct fi_eq_err_entry *buf, uint64_t flags)
-{
-	return -FI_ENOSYS;
 }
 
 static inline ssize_t ofi_nd_eq_ev2buf(nd_eq_event_t *ev,
@@ -183,24 +184,132 @@ static inline ssize_t ofi_nd_eq_ev2buf(nd_eq_event_t *ev,
 	return (ssize_t)copylen;
 }
 
+
+static ssize_t ofi_nd_eq_read(struct fid_eq *peq, uint32_t *pev,
+			      void *buf, size_t len, uint64_t flags)
+{
+	nd_eq_t *eq = container_of(peq, nd_eq_t, fid);
+
+	DWORD bytes = 0;
+	ULONG_PTR key = 0;
+	OVERLAPPED *ov = 0;
+	ssize_t res = 0;
+
+	nd_eq_event_t *ev = 0;
+
+	if (!eq->count)
+		return -FI_EAGAIN;
+
+	/* we have to use critical section here because concurrent thread
+	   may read event with FI_PEEK flag */
+	EnterCriticalSection(&eq->lock);
+
+	/* check again because it may be changed on critical section barrier */
+	if (!eq->count)
+	{
+		LeaveCriticalSection(&eq->lock);
+		return -FI_EAGAIN;
+	}
+
+	if (eq->peek)
+	{
+		ev = eq->peek;
+	}
+	else
+	{
+		assert(eq->iocp);
+		if (GetQueuedCompletionStatus(eq->iocp, &bytes, &key, &ov, 0))
+		{
+			ev = container_of(ov, struct nd_eq_event, ov);
+		}
+	}
+
+	/* in case if no event available, but counter is non-zero - error available */
+	if (!ev && eq->count)
+	{
+		LeaveCriticalSection(&eq->lock);
+		return -FI_EAVAIL;
+	}
+
+	res = ofi_nd_eq_ev2buf(ev, buf, len);
+	*pev = ev->eq_event;
+
+	if (flags & FI_PEEK)
+	{
+		eq->peek = ev;
+		/* we updated peek ptr, notify other waiters about this */
+		WakeByAddressAll((void*)&eq->count);
+	}
+	else
+	{
+		eq->peek = NULL;
+		InterlockedDecrement(&eq->count);
+		assert(eq->count >= 0);
+	}
+
+	return res;
+}
+
+static ssize_t ofi_nd_eq_readerr(struct fid_eq *peq,
+				 struct fi_eq_err_entry *buf, uint64_t flags)
+{
+	nd_eq_t *eq = container_of(peq, nd_eq_t, fid);
+
+	DWORD bytes = 0;
+	ULONG_PTR key = 0;
+	OVERLAPPED *ov = 0;
+
+	nd_eq_event_t *ev = 0;
+
+	if (!eq->errdata)
+	{
+		free(eq->errdata);
+		eq->errdata = 0;
+	}
+
+	if (!GetQueuedCompletionStatus(eq->err, &bytes, &key, &ov, 0))
+		return -FI_EAGAIN;
+
+	InterlockedDecrement(&eq->count);
+	ev = container_of(ov, nd_eq_event_t, ov);
+
+	if (buf->err_data && buf->err_data_size)
+	{
+		memcpy(buf, &ev->error, offsetof(struct fi_eq_err_entry, err_data));
+		buf->err_data_size = MIN(buf->err_data_size, ev->error.err_data_size);
+		memcpy(buf->err_data, ev->error.err_data, buf->err_data_size);
+		/* to be sure that the errdata in EQ is NULL */
+		eq->errdata = 0;
+	}
+	else
+	{
+		/* for compatibility purposes (release < 1.5 or passed err_data_size is 0) */
+		memcpy(buf, &ev->error, sizeof(ev->error));
+		eq->errdata = ev->error.err_data;
+	}
+
+	return FI_SUCCESS;
+}
+
 static ssize_t ofi_nd_eq_sread(struct fid_eq *peq, uint32_t *pev,
 			       void *buf, size_t len, int timeout,
 			       uint64_t flags)
 {
-	nd_eq_t *nd_eq_ptr = container_of(peq, struct nd_eq, fid);
+	nd_eq_t *nd_eq_ptr = container_of(peq, nd_eq_t, fid);
 	ssize_t res = 0;
 	int retry = 1;
+	LONG zero = 0;
 	while(retry)
 	{
-		nd_eq_ptr->count = 0;
-		while (!nd_eq_ptr->count)
+		do
 		{
-			LONG zero = 0;
 			if (!WaitOnAddress(
 				&nd_eq_ptr->count, &zero, sizeof(nd_eq_ptr->count),
 				(DWORD)timeout) && timeout >= 0)
+			{
 				return -FI_EAGAIN;
-		}
+			}
+		} while (!nd_eq_ptr->count);
 
 		/* we have to use critical section here because concurrent thread
 		may read event with FI_PEEK flag */
@@ -270,13 +379,52 @@ static ssize_t ofi_nd_eq_sread(struct fid_eq *peq, uint32_t *pev,
 static const char *ofi_nd_eq_strerror(struct fid_eq *eq, int prov_errno,
 				      const void *err_data, char *buf, size_t len)
 {
-	return NULL;
+	if (buf && len)
+		return strncpy(buf, fi_strerror(-prov_errno), len);
+	return fi_strerror(-prov_errno);
+}
+
+static inline void ofi_nd_eq_free_event(nd_eq_event_t *ev)
+{
+	if (ev->data)
+		free(ev->data);
+
+	if (ev->eq_event == FI_CONNREQ)
+	{
+		struct fi_eq_cm_entry *cm = (struct fi_eq_cm_entry*)&ev->operation;
+		if (cm->info)
+			fi_freeinfo(cm->info);
+	}
+
+	free(ev);
 }
 
 static ssize_t ofi_nd_eq_write(struct fid_eq *peq, uint32_t ev,
 			       const void *buf, size_t len, uint64_t flags)
 {
-	return -FI_ENOSYS;
+	nd_eq_t *eq = container_of(peq, nd_eq_t, fid);
+
+	nd_eq_event_t *custom = (nd_eq_event_t*)calloc(1, sizeof(*custom));
+	if (!custom)
+		return -FI_ENOMEM;
+
+	custom->is_custom = 1;
+	custom->eq_event = ev;
+	if (len)
+	{
+		custom->data = malloc(len);
+		if (!custom->data)
+		{
+			ofi_nd_eq_free_event(custom);
+			return -FI_ENOMEM;
+		}
+		custom->len = len;
+		memcpy(custom->data, buf, len);
+	}
+
+	ofi_nd_eq_push(eq, custom);
+
+	return len;
 }
 
 #endif /* _WIN32 */
